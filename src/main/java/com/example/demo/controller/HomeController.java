fb5e7c30ecf30a741248e10fff21597c27438213
Nagesh9890 <nagesh9500@gmail.com>
2023-07-10 14:02:23 +0530

    initial commit

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HomeController {
+
+	@GetMapping("/get")
+	public String home() {
+
+		return "Hello Spring Boot Docker";
+
+	}
+
+}
